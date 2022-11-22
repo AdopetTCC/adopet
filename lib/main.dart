658bd2823ca52a22599fdc6357f72671dfac9e19
AdopetTCC <adopettcc@gmail.com>
2022-11-21 22:56:17 -0300

    images

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:adopet/screens/animal_screen.dart';
 import 'package:adopet/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const RegisterScreen(),
+      home: const AnimalScreen(),
     );
   }
 }
