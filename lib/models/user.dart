cd945fb2f3c3638e00323f37535e15528c297dd2
AdopetTCC <adopettcc@gmail.com>
2022-12-02 22:56:02 -0300

    user commit

---
+++ lib/models/user.dart
@@ -0,0 +1,25 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String name;
+  final String uid;
+  final String photoUrl;
+  final String email;
+  final String telefone;
+
+  const User({
+    required this.name,
+    required this.uid,
+    required this.photoUrl,
+    required this.email,
+    required this.telefone,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "uid": uid,
+        "photoUrl": photoUrl,
+        "sex": email,
+        "age": telefone,
+      };
+}
